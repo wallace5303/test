d2c37fbcc04c7837e7e1e0d0af9f7c7092911aad
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-09-27 17:47:25 +0800

    1

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "beego-demo/routers"
+	_ "test/routers"
 
 	beego "github.com/beego/beego/v2/server/web"
 )
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"beego-demo/controllers"
+	"test/controllers"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
